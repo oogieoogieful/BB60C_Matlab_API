8a3a41e62977bc0c40f053fd5507691d0af61199
oogieoogieful <erin.wiles@gmail.com>
2015-07-09 10:52:49 -0600

    fixed colorbar label

--- printer.m
+++ printer.m
@@ -11,37 +11,34 @@ freq_bins=length(max_mat);
 freq_vec=linspace(start,stop,freq_bins);%Hz
 
 % Convert linear to log
-max_mat_log=-abs(10.*log10(max_mat)); %losing precision when convert
-% mean_mat_log=-abs(10.*log10(mean_mat));
-% std_mat_log=-abs(10.*log10(std_mat));
-
-
-minutes_saved=30;% make this more general, save in compress
-num_frame=length(sample_time);
+max_mat_log=-abs(10.*log10(max_mat)); %losing precision when convert?
+mean_mat_log=-abs(10.*log10(mean_mat));
+std_mat_log=-abs(10.*log10(std_mat));
 
 % Spectrogram
 figure(1)
 imagesc(freq_vec/1e6,1:minutes_saved,max_mat_log)
-cmin=-90;%dBm
-cmax=-40;%dBm
-caxis([cmin cmax])
-colorbar()
-c.Label.String='Power [dBm]';
 xlabel('Frequency [MHz]')
 ylabel('Time [Minutes]')
 title(time_stamp)
+cmin=-90;%dBm
+cmax=-40;%dBm
+caxis([cmin cmax])
+h=colorbar();
+xlabel(h,'Power [dBm]')
+
 
 % Compress 30 min. recording to one frame
 figure(2)
 hold on;
 plot(freq_vec/1e6,max(max_mat_log),'b')
-%plot(freq_vec/1e6,mean(mean_mat_log),'m')
+plot(freq_vec/1e6,mean(mean_mat_log),'m')
 %plot(freq_vec,max(mean_mat_log),'k')
-axis([0 1000 -100 0])
+axis([0 1000 -105 0])
 xlabel('Frequency [MHz]')
 ylabel('Power [dBm]')
 title(time_stamp)
-legend('Max of Max Trace of 15 sec period for 30 min. recording','Mean of 30 min. recording','Max of Mean')
+legend('Max ...','Mean ...','Max of Mean')
 hold off;
 
 % histogram
