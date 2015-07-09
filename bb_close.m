681acee4047203e930d1023f321ef4752f2f098f
oogieoogieful <erin.wiles@gmail.com>
2015-07-09 15:55:09 -0600

    comments

--- bb_close.m
+++ bb_close.m
@@ -1,2 +1,5 @@
+% function bb_close
+% clear;clc;close all;
 % CLOSE DEVICE
-connect_msg(2,:)=calllib('bb_api','bbCloseDevice',device_ptr.Value);
+connect_msg(2,:)=calllib('bb_api','bbCloseDevice',deviceValue)
+%connect_msg(1,:)=calllib('bb_api','bbCloseDevice',0)
--- bb_fetch.m
+++ bb_fetch.m
@@ -1,10 +1,10 @@
 close all;clear;clc;
 % Fetch a single trace
 bb_open;
-min_ptr=libpointer('doublePtr',zeros(1,traceLen_ptr.Value));
-max_ptr=libpointer('doublePtr',zeros(1,traceLen_ptr.Value));
-trace=calllib('bb_api','bbFetchTrace',device_ptr.Value,traceLen_ptr.Value,min_ptr,max_ptr);
-plot(max_ptr.Value)
+minPtr=libpointer('doublePtr',zeros(1,traceLenPtr.Value));
+maxPtr=libpointer('doublePtr',zeros(1,traceLenPtr.Value));
+trace=calllib('bb_api','bbFetchTrace',devicePtr.Value,traceLenPtr.Value,minPtr,maxPtr);
+plot(maxPtr.Value)
 ylabel('Power')
 xlabel('Indices')
 bb_close;
